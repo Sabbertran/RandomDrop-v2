a4eb57a3c4162ccb150a95e05e8f2b991f6c64fc
Sabbertran <sabbertran@live.de>
2014-05-26 11:04:06 +0200

    Setting up the basic environment to work with

---
+++ src/me/sabbertran/randomdropv2/region/Region.java
@@ -0,0 +1,13 @@
+package me.sabbertran.randomdropv2.region;
+
+import me.sabbertran.randomdropv2.RandomDrop;
+import org.bukkit.World;
+
+public class Region
+{
+
+    private RandomDrop main;
+
+    private World world;
+    private int minX, minY, minZ, maxX, maxY, maxZ;
+}
