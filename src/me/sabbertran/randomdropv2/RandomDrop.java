208ee77b001d554cae177e8077018e6f6eb3480b
Sabbertran <sabbertran@live.de>
2014-05-26 11:04:16 +0200

    Setting up the basic environment to work with

--- src/me/sabbertran/randomdropv2/RandomDrop.java
+++ src/me/sabbertran/randomdropv2/RandomDrop.java
@@ -6,17 +6,37 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class RandomDrop extends JavaPlugin
 {
+
+    private Events events;
+
+    private boolean dropsEnabled;
+    private int globalDropChance;
+
     private static final Logger log = Bukkit.getLogger();
-    
+
     @Override
     public void onDisable()
     {
         log.info("RandomDrop v2 disabled");
     }
-    
+
     @Override
     public void onEnable()
     {
+
+        events = new Events(this);
+        getServer().getPluginManager().registerEvents(events, this);
+
         log.info("RandomDrop v2 enabled");
+    }
+
+    public int getGlobalDropChance()
+    {
+        return globalDropChance;
+    }
+
+    public boolean getDropsEnabled()
+    {
+        return dropsEnabled;
     }
 }
